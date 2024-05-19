1956a684adc76aad8ba7bb48bec00919af05df66
Emily <egrout@ab.mpg.de>
2024-05-19 12:31:17 +0200

    looking at durations of splits and merge
    still need to do debugging of GPS error

--- fission_fusion_lion.R
+++ fission_fusion_lion.R
@@ -9,19 +9,10 @@
 #--------PARAMS-------
 data_dir <- "C:/Users/egrout/Dropbox/lion/data/processed/"
 code_dir <- 'C:/Users/egrout/Dropbox/lion/Lion_code/'
-coati_dir <- 'C:/Users/egrout/Dropbox/coatithon/coatithon_code/code_review/'
+coati_dir <- 'C:/Users/egrout/Dropbox/coatithon/coatithon_code/'
 plot_dir <- 'C:/Users/egrout/Dropbox/lion/results/level0/'
 gps_file <- "lion_xy_15min_level0.RData" #level0 is when Venus is not removed
 id_file <- 'lion_ids.RData'
-
-#list of Rs
-Rs <- c(10,20,30,40,50,100)
-Rs <- c(100,200,300,400,500,1000)
-Rs <- c(1000,2000,3000,4000,5000,10000)
-Rs <- c(10000,20000,30000,40000,50000,100000)
-
-
-R <- 50
 
 #-------SETUP-------
 
@@ -40,7 +31,7 @@ library(patchwork)
 
 #read in library of functions
 setwd(coati_dir)
-source('coati_function_library_V1.R')
+source('coati_function_library.R')
 
 #load data
 setwd(data_dir)
@@ -68,10 +59,16 @@ all_tracked_idxs <- which(n_tracked==n_inds)
 
 #----------------
 #looking at the sub grouping patterns
+
+#list of Rs
+Rs <- c(10,20,30,40,50,100)
+Rs <- c(100,200,300,400,500,1000)
+Rs <- c(1000,2000,3000,4000,5000,10000)
+Rs <- c(10000,20000,30000,40000,50000,100000)
+
 #Number of sub groups when the radius is changed (graph put in dropbox results folder)
 
 png(height = 2200, width = 900, units = 'px', filename = paste0(plot_dir,'n_subgroups_hists_10000-100000m.png'))
-
 par(mfrow=c(6,1), mar = c(8,9,12,1), mgp=c(6,1,0)) #(bottom, left, top, right)
 
 for (i in 1:length(Rs)){
@@ -94,7 +91,7 @@ dev.off()
 
 #looking at which individuals split with one another
 
-R = 10
+R = 50000
 subgroup_data <- get_subgroup_data(xs, ys, R)
 
 ff_net <- matrix(NA, nrow = n_inds, ncol = n_inds)
@@ -116,9 +113,9 @@ diag(ff_net) <- NA
 new_order <- c(1:3,5,4,6)
 ffnet_reorder <- ff_net[new_order, new_order]
 
-png(height = 600, width = 650, units = 'px', filename = paste0(plot_dir,'subgroup_network_10m.png'))
+png(height = 600, width = 650, units = 'px', filename = paste0(plot_dir,'subgroup_network_50000m.png'))
 
-visualize_network_matrix_galaxy(ffnet_reorder, lion_ids[new_order,])
+visualize_network_matrix_trago(ffnet_reorder, lion_ids[new_order,])
 dev.off()
 
 #it looks like Kiara and Sarafina split from the other group members and travelled atleast 50km from them
@@ -147,7 +144,7 @@ sd(each_sum$prop)
 
 #what is the absolute diadic distance of the group when the group is together
 #saying "together" is when they're 100m from one another
-R = 1000
+R = 20
 subgroup_data <- get_subgroup_data(xs, ys, R)
 
 #find index for when there is only 1 subgroup
@@ -165,13 +162,21 @@ within_group_data <- get_proximity_data(subset_x, subset_y, 3)
 new_order <- c(1:3,5,4,6)
 ffnet_reorder <- ff_net[new_order, new_order]
 
-png(height = 600, width = 650, units = 'px', filename = paste0(plot_dir,'within_1000m_subgroup_network_3m.png'))
-visualize_network_matrix_galaxy(within_group_data$proximity_net, lion_ids[new_order,])
+png(height = 600, width = 650, units = 'px', filename = paste0(plot_dir,'within_20m_subgroup_network_3m.png'))
+visualize_network_matrix_trago(within_group_data$proximity_net, lion_ids[new_order,])
 dev.off()
 
 #--------------------------------------------------------
 
 #now want to work out how long they were apart for...
+
+# this code may not be right since the function is for 1Hz data
+# ff_data_200 <- detect_fissions_and_fusions(R_inner = 50, R_outer = 200, xs, ys, ts, lion_ids)
+# ff_events <- ff_data_200$events_detected
+# #removing wonky events
+# ff_events <- ff_events[!ff_events$group_A_idxs == "0",]
+# analyse_ff_event(7, events = ff_events, xs, ys, ts, max_time = 15)
+
 
 #made splits_df and merge_df in lion_split_merge_dfs script
 
@@ -179,7 +184,6 @@ load(file = paste0(data_dir, "lion_splits_df.Rdata"))
 load(file = paste0(data_dir, "lion_merge_df.Rdata"))
 
 #rbind the dataframes to calculate the duration between fissions and fusions
-
 merge_df$event <- "merge"
 splits_df$event <- "split"
 
@@ -240,6 +244,12 @@ colnames(split_merge_df) <- c("t", "orig_group", "sub1", "sub2", "sub3", "sub4",
 #order the dataframe by time
 split_merge_df <- split_merge_df[(order(split_merge_df$t)),]
 
+#remove duplicates
+split_merge_df<- split_merge_df[!duplicated(split_merge_df[,1:5]),]
+
+#change index of the rows to time order
+rownames(split_merge_df) <- 1:nrow(split_merge_df)
+
 #add the time of each event
 split_merge_df$datetime <- ts[split_merge_df$t]
 split_merge_df$round_time <- round_date(split_merge_df$datetime, "1 hour")
@@ -249,7 +259,7 @@ split_merge_df$hour <- as.numeric(split_merge_df$hour)
 
 
 p1 <- ggplot(split_merge_df, aes(x=hour, fill=event)) +
-  geom_histogram(alpha=0.5, position="dodge", aes(y=..density..), bins=24)+
+  geom_histogram(alpha=0.5, position="dodge", bins=24)+
   scale_fill_manual(values = c("turquoise3", "dodgerblue4"))+
   scale_color_brewer(palette="Accent")+ 
   guides(fill="none")+
@@ -272,5 +282,139 @@ gg <- (p1 | p2) / p3
 gg
 
 ggsave(filename = paste0(plot_dir, 'duration_descriptions.png'), plot = gg, width = 10, height = 5, dpi = 300)
+
+#want to plot the distance groups travelled against duration apart
+
+#making new columns to add duration and max dist apart info for plotting
+# Making new columns to add duration and max dist apart info for plotting
+split_merge_df$dyad_dist_max_1.2 <- NA
+split_merge_df$dyad_dist_max_1.3 <- NA
+split_merge_df$dyad_dist_max_2.3 <- NA
+
+split_merge_df$event_dur <- NA
+
+# Loop through each row of the dataframe
+for (i in 1:(nrow(split_merge_df) - 1)) {
+  
+  # Print debug information
+  cat("Processing row:", i, "\n")
+  
+  # Check if the current index is less than the total number of rows minus 1
+  if (i < nrow(split_merge_df)) {
+    
+    # Extract time range for the current and next event
+    time_range <- split_merge_df$t[i]:split_merge_df$t[i+1]
+    
+    # Check if the time range is valid
+    if (length(time_range) > 0 && split_merge_df$t[i] < split_merge_df$t[i+1]) {
+      
+      # Group 1 coordinates
+      group_1_xs <- xs[split_merge_df$sub1[[i]], time_range, drop = FALSE]
+      group_1_ys <- ys[split_merge_df$sub1[[i]], time_range, drop = FALSE]
+      
+      mean_grp1_xs <- colMeans(group_1_xs, na.rm = TRUE)
+      mean_grp1_ys <- colMeans(group_1_ys, na.rm = TRUE)
+      
+      # Group 2 coordinates
+      group_2_xs <- xs[split_merge_df$sub2[[i]], time_range, drop = FALSE]
+      group_2_ys <- ys[split_merge_df$sub2[[i]], time_range, drop = FALSE]
+      
+      mean_grp2_xs <- colMeans(group_2_xs, na.rm = TRUE)
+      mean_grp2_ys <- colMeans(group_2_ys, na.rm = TRUE)
+      
+      # Group 3 coordinates
+      group_3_xs <- xs[split_merge_df$sub3[[i]], time_range, drop = FALSE]
+      group_3_ys <- ys[split_merge_df$sub3[[i]], time_range, drop = FALSE]
+      
+      mean_grp3_xs <- colMeans(group_3_xs, na.rm = TRUE)
+      mean_grp3_ys <- colMeans(group_3_ys, na.rm = TRUE)
+      
+      
+      # Get distance between centroids of subgroup 1 and 2
+      dyad_dist <- sqrt((mean_grp1_xs - mean_grp2_xs)^2 + (mean_grp1_ys - mean_grp2_ys)^2)
+      dyad_dist_max_1.2 <- max(dyad_dist, na.rm = TRUE)
+      
+      # Get distance between centroids of subgroup 1 and 3
+      dyad_dist <- sqrt((mean_grp1_xs - mean_grp3_xs)^2 + (mean_grp1_ys - mean_grp3_ys)^2)
+      dyad_dist_max_1.3 <- max(dyad_dist, na.rm = TRUE)
+      
+      # Get distance between centroids of subgroup 2 and 3
+      dyad_dist <- sqrt((mean_grp2_xs - mean_grp3_xs)^2 + (mean_grp2_ys - mean_grp3_ys)^2)
+      dyad_dist_max_2.3 <- max(dyad_dist, na.rm = TRUE)
+      
+      
+      # Update dataframe with max distance and event duration
+      split_merge_df$dyad_dist_max_1.2[i] <- dyad_dist_max_1.2
+      split_merge_df$dyad_dist_max_1.3[i] <- dyad_dist_max_1.3
+      split_merge_df$dyad_dist_max_2.3[i] <- dyad_dist_max_2.3
+      
+      #split_merge_df$event_dur[i] <- (split_merge_df$t[i+1] - split_merge_df$t[i]) * 15
+      split_merge_df$event_dur[i] <- difftime(split_merge_df$datetime[i + 1], split_merge_df$datetime[i], units = "mins")
+      
+    } else {
+      cat("Invalid time range at row:", i, "\n")
+    }
+  }
+}
+
+#plotting one of the events to see the trajectories
+j <- 1:ncol(group_1_xs)
+
+xmax <- max(na.omit(c(group_1_xs, group_2_xs)))
+xmin <- min(na.omit(c(group_1_xs, group_2_xs)))
+ymax <- max(na.omit(c(group_1_ys, group_2_ys)))
+ymin <- min(na.omit(c(group_1_ys, group_2_ys)))
+
+plot(group_1_xs[,j], group_1_ys[,j], type = "l", col = "red", xlim = c(xmax, xmin), ylim = c(ymax,ymin))
+points(group_2_xs[,j], group_2_ys[,j], type = "l", col = "blue")
+
+
+#finding events where split goes to merge to plot the distance/time plot
+
+# Initialize a logical vector to keep desired rows
+keep_rows <- rep(FALSE, nrow(split_merge_df))
+
+# Initialize a variable to track the start of a "split" sequence
+split_started <- FALSE
+split_index <- NA
+
+# Loop through the dataframe to identify the first "split" and the first "merge" in each sequence
+for (i in 1:(nrow(split_merge_df) - 1)) {
+  if (split_merge_df$event[i] == "split" && !split_started) {
+    # Mark the first split in the sequence
+    split_started <- TRUE
+    split_index <- i
+  }
+  
+  if (split_started && split_merge_df$event[i + 1] == "merge") {
+    # Mark the first merge after the split sequence
+    keep_rows[split_index] <- TRUE
+    keep_rows[i + 1] <- TRUE
+    
+    # Reset tracking variables for the next sequence
+    split_started <- FALSE
+    split_index <- NA
+  }
+}
+
+# Filter the dataframe
+filtered_split_merge_df <- split_merge_df[keep_rows, ]
+
+#getting the duration in days for plotting
+filtered_split_merge_df$event_dur_day <- (filtered_split_merge_df$event_dur/60)/24
+
+#Duration groups were split
+ggplot(filtered_split_merge_df[filtered_split_merge_df$event == "split",], aes(x = event_dur_day, y = dyad_dist_max_1.2)) +
+  geom_point() +
+  xlab("Duration (days)") +
+  ylab("Distance Apart (m)") +
+  theme_classic()
+
+#durations within the day
+
+
+#I'm not sure the dyadic distances are correct here, so need to make gganimate plots for each event to see what's happening.... especially for the merges which have a huge dyadic distance e.g on 2023-11-07 17:45:00
+#could be GPS error! for i = 333 (same as time written here)
+
 
 
